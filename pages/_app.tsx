f2d9fa73ca8042c010ed29aaea65e2f75085cc97
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-10 13:37:57 -0300

    feat: configured provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import { store } from "../src/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
