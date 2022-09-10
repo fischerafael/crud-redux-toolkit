7e2fc202b6789c093bbb1a875deb031eb60769ba
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-10 14:38:28 -0300

    feat: completed todo crud

--- src/pages/ReduxCRUD.tsx
+++ src/pages/ReduxCRUD.tsx
@@ -1,21 +1,62 @@
+import { useState } from "react";
+import { addTodo, ITodo, removeTodo, updateTodo } from "../store/todoSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+
+const createUniqueID = () => {
+  return String(Math.random());
+};
+
+const currentTodoInitialState = {
+  id: "",
+  isCompleted: false,
+  title: "",
+};
+
 export const ReduxCRUD = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state: RootState) => state.todos);
+
+  const [currentTodo, setCurrentTodo] = useState<ITodo>(
+    currentTodoInitialState
+  );
+
+  const handleAddTodo = () => {
+    if (!currentTodo.title) return;
+    dispatch(addTodo({ ...currentTodo, id: createUniqueID() }));
+    setCurrentTodo(currentTodoInitialState);
+  };
+
+  const handleRemoveTodo = (todo: ITodo) => {
+    dispatch(removeTodo(todo));
+  };
+
+  const handleUpdateTodo = (todo: ITodo) => {
+    dispatch(updateTodo(todo));
+  };
+
   return (
     <div>
       <div>
-        <input placeholder="Todo" />
-        <button>Criar Todo</button>
+        <input
+          placeholder="Todo"
+          value={currentTodo?.title}
+          onChange={(e) =>
+            setCurrentTodo({ ...currentTodo, title: e.target.value })
+          }
+        />
+        <button onClick={handleAddTodo}>Criar Todo</button>
       </div>
       <ul>
-        <li style={{ display: "flex", width: "full" }}>
-          <p>Estudar React</p>
-          <button>Completar</button>
-          <button>Remover</button>
-        </li>
-        <li style={{ display: "flex", width: "full" }}>
-          <p>Estudar React</p>
-          <button>Completar</button>
-          <button>Remover</button>
-        </li>
+        {todos.todos.map((todo) => (
+          <li key={todo.id} style={{ display: "flex", width: "full" }}>
+            <p>{todo.title}</p>
+            <button onClick={() => handleUpdateTodo(todo)}>
+              {todo.isCompleted ? "Não completo" : "Completo"}
+            </button>
+            <button onClick={() => handleRemoveTodo(todo)}>Remover</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/store/todoSlice/index.ts
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: ITodoState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<ITodo>) => {
+      const todosUpdated = [...state.todos, action.payload];
+      state.todos = todosUpdated;
+    },
+
+    removeTodo: (state, action: PayloadAction<ITodo>) => {
+      const todosUpdated = state.todos.filter(
+        (todo) => todo.id !== action.payload.id
+      );
+      state.todos = todosUpdated;
+    },
+
+    updateTodo: (state, action: PayloadAction<ITodo>) => {
+      const todoUpdate = state.todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          const updatedTodo: ITodo = {
+            id: todo.id,
+            title: todo.title,
+            isCompleted: todo.isCompleted === true ? false : true,
+          };
+          return updatedTodo;
+        }
+        return todo;
+      });
+      state.todos = todoUpdate;
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+export default todoSlice.reducer;
+
+export interface ITodo {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
+export interface ITodoState {
+  todos: ITodo[];
+}
